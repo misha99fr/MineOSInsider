199ba5cd42dda4579e6e21cd66f14dc4127ae5a7
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2024-08-08 18:12:35 +0300

    ХУЙ

--- main.lua
+++ main.lua
@@ -4,15 +4,15 @@ local system = require("System")
 local workspace, window, menu = system.addWindow(GUI.titledWindow(1, 1, 50, 10, "Приветствие", true))
 
 local textBox = window:addChild(GUI.textBox(2, 2, 46, 5, nil, 0x000000, {
-    "Добро пожаловать в приложение Lenivo Vantage, к сожалению к ы и пловылпы."
+    "Добро пожаловать в приложение MineOS Insider, здесь мы рассказываем о новых изменениях MineOS"
 }, 1, 0, 0, true, true))
 
 -- Добавление кнопки
-local button = window:addChild(GUI.button(2, 8, 46, 3, 0xCCCCCC, 0x000000, 0xAAAAAA, 0x000000, "Что это?"))
+local button = window:addChild(GUI.button(2, 8, 46, 3, 0xCCCCCC, 0x000000, 0xAAAAAA, 0x000000, "Что нового?"))
 
 -- Обработчик события для кнопки
 button.onTouch = function()
-    GUI.alert("Это специально по, которое будет устанавливаться на специальные ноутбуки Lenivo(TM), пока что я его делаю и чета пробую")
+    GUI.alert("В MineOS App Market, добавили возможность постить скриншоты на странице с приложением/игрой")
 end
 
 window.actionButtons.close.onTouch = function()
